12edac10926fa5cc51fb3ac1740e04f59a60df05
art-m <Art-malinkov@yandex.ru>
2025-04-28 13:32:47 +0300

    Прямое соединение виджетов между собой

--- src/develop.py
+++ src/develop.py
@@ -1,46 +1,33 @@
 import sys
-from random import choice
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
-
-window_titles = [
-    "My App",
-    "My App",
-    "Still My App",
-    "Still My App",
-    "What on earth",
-    "What on earth",
-    "This is surprising",
-    "This is surprising",
-    "Something went wrong",
-]
+from PySide6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QVBoxLayout,
+    QWidget,
+)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.n_times_clicked = 0
-
         self.setWindowTitle("My App")
 
-        self.button = QPushButton("Press Me!")
-        self.button.clicked.connect(self.the_button_was_clicked)
+        self.label = QLabel()
 
-        self.windowTitleChanged.connect(self.the_window_title_changed)
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
 
-        self.setCentralWidget(self.button)
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
 
-    def the_button_was_clicked(self):
-        print("Clicked.")
-        new_window_title = choice(window_titles)
-        print("Setting title:  %s" % new_window_title)
-        self.setWindowTitle(new_window_title)
+        container = QWidget()
+        container.setLayout(layout)
 
-    def the_window_title_changed(self, window_title):
-        print("Window title changed: %s" % window_title)
-
-        if window_title == "Something went wrong":
-            self.button.setDisabled(True)
+        self.setCentralWidget(container)
 
 app = QApplication(sys.argv)
 
