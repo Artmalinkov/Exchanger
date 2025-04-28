5b3c853a0e72c2b11b0055f0b7cebe1a724102f5
art-m <Art-malinkov@yandex.ru>
2025-04-28 14:05:51 +0300

    label

--- src/develop.py
+++ src/develop.py
@@ -1,65 +1,37 @@
 import sys
 
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
     QApplication,
     QCheckBox,
     QComboBox,
-    QDateEdit,
-    QDateTimeEdit,
     QDial,
     QDoubleSpinBox,
-    QFontComboBox,
     QLabel,
-    QLCDNumber,
     QLineEdit,
+    QListWidget,
     QMainWindow,
-    QProgressBar,
-    QPushButton,
-    QRadioButton,
     QSlider,
     QSpinBox,
-    QTimeEdit,
-    QVBoxLayout,
-    QWidget,
 )
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Widgets App")
+        self.setWindowTitle("My App")
 
-        layout = QVBoxLayout()
-        widgets = [
-            QCheckBox,
-            QComboBox,
-            QDateEdit,
-            QDateTimeEdit,
-            QDial,
-            QDoubleSpinBox,
-            QFontComboBox,
-            QLCDNumber,
-            QLabel,
-            QLineEdit,
-            QProgressBar,
-            QPushButton,
-            QRadioButton,
-            QSlider,
-            QSpinBox,
-            QTimeEdit,
-        ]
+        label = QLabel("Hello")
+        font = label.font()
+        font.setPointSize(30)
+        label.setFont(font)
+        label.setAlignment(
+            Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter
+        )
 
-        for widget in widgets:
-            layout.addWidget(widget())
-
-        central_widget = QWidget()
-        central_widget.setLayout(layout)
-
-        self.setCentralWidget(central_widget)
+        self.setCentralWidget(label)
 
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
 app.exec()
-
-
